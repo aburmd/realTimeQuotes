3824d3226a2ea4d1dddf2a5f44ee1386ad6e4df9
Mohamed Ibrahim <aburmd@gmail.com>
2024-01-22 05:09:13 -0800

    local path added

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import os
 
-os.system("python3 mainRealQuotes.py &")
-os.system("python3 mainSendmail.py &")
+env=os.environ['en']
+
+if env=='local':
+    os.system("/usr/bin/python3 /Users/abuhura/Desktop/desktop_19thSep2023/Training/workspace/realTimeQuotes/mainRealQuotes.py &")
+    os.system("/usr/bin/python3 /Users/abuhura/Desktop/desktop_19thSep2023/Training/workspace/realTimeQuotes/mainSendmail.py &")
+else:
+    os.system("/usr/bin/python3 /home/ec2-user/workspace/realTimeQuotes/mainRealQuotes.py &")
+    os.system("/usr/bin/python3 /home/ec2-user/workspace/realTimeQuotes/mainSendmail.py &")
