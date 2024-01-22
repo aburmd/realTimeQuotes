afa8015cf5e28f14645c39c53fa33810fe7c5d3f
Mohamed Ibrahim <aburmd@gmail.com>
2024-01-22 03:19:16 -0800

    change it to current date

--- mainRealQuotes.py
+++ mainRealQuotes.py
@@ -6,12 +6,13 @@ import getStored
 
 api_val=auth.getAPIKey('poly')
 sqClient=SQ(api_key=api_val)
-'''
+
 start_dt=formater.getCurDate()
 end_dt=formater.getCurDate()
 '''
 start_dt='2024-01-16'
 end_dt='2024-01-16'
+'''
 
 data=realQuote.getQuote(start_dt,end_dt)
 getStored.fileInput(data['results'])
