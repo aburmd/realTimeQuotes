4f58561e7ee85a03bd2eaa43d8424cbe21667718
Mohamed Ibrahim <aburmd@gmail.com>
2024-01-24 03:56:04 -0800

    change scheduler to run exactly at 0,30 mins in every hour

--- pyscd.py
+++ pyscd.py
@@ -1,11 +1,11 @@
 import os
+import schedule
 import time
-from schedule import every, repeat, run_pending
 
 env=os.environ['en']
 
-@repeat(every(30).minutes)
 def job():
+    print("Job executed at:", time.strftime("%Y-%m-%d %H:%M:%S"))
     if env=='local':
         os.system("/usr/bin/python3 /Users/abuhura/Desktop/desktop_19thSep2023/Training/workspace/realTimeQuotes/mainRealQuotes.py &")
         os.system("/usr/bin/python3 /Users/abuhura/Desktop/desktop_19thSep2023/Training/workspace/realTimeQuotes/mainSendmail.py &")
@@ -13,6 +13,9 @@ def job():
         os.system("/usr/bin/python3 /home/ec2-user/workspace/realTimeQuotes/mainRealQuotes.py &")
         os.system("/usr/bin/python3 /home/ec2-user/workspace/realTimeQuotes/mainSendmail.py &")
         
+schedule.every().hour.at(":00").do(job)
+schedule.every().hour.at(":30").do(job)
+
 while True:
-    run_pending()
+    schedule.run_pending()
     time.sleep(1)
